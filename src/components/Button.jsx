82920c8556e6a9db6fc46999cab77bdf5e55387a
Cole Cline <113210244+colecline22@users.noreply.github.com>
2022-11-08 10:42:18 -0600

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,11 +6,8 @@ export default function Button() {
       <Popover.Button>Solutions</Popover.Button>
 
       <Popover.Panel className="absolute z-10">
-        <div className="grid grid-cols-2">
-          <a href="/analytics">Analytics</a>
-          <a href="/engagement">Engagement</a>
-          <a href="/security">Security</a>
-          <a href="/integrations">Integrations</a>
+        <div>
+          <p>Hello</p>
         </div>
       </Popover.Panel>
     </Popover>
